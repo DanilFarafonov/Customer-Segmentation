f1ed7f7b20855d1cd2ef4caeb771f4d2d114f77e
Danil_Farafonov <faraphonovd@yandex.ru>
2023-04-10 22:20:08 +0500

    add functions to load and filter data

--- data_creation.py
+++ data_creation.py
@@ -1,18 +1,43 @@
 import pandas as pd
 from btyd.utils import summary_data_from_transaction_data
-from sklearn.model_selection import train_test_split
+from datetime import date, timedelta
+from calendar import monthrange
 
-# читаем файл с транзакционными данными с помощью pandas
-df_raw = pd.read_parquet("data/wallet_urfu.parquet.gzip")
 
-# перевод транзакционной истории в сводные данные
-df_summary = summary_data_from_transaction_data(transactions=df_raw, customer_id_col="partner",
-                                                datetime_col="rep_date", monetary_value_col="monetary")
+def get_end_of_period_date(year: int, month: int):
+    """
+    Returns the end date of the billing period
+    :param year: int, end year of the billing period
+    :param month: int, month of the end of the billing period
+    :return: datetime.date
+    """
+    date_current = date(year, month, 1)
+    days_in_current_month = monthrange(year, month)[1]
+    date_end = date_current + timedelta(days=days_in_current_month)
+    return date_end
 
-# разбивка на обучающую и тестовую выборки
-df_train, df_test = train_test_split(df_summary, test_size=0.3, random_state=42)
 
-# сохранение датафреймов в csv
-df_summary.to_csv("data/data_summary.csv", encoding="utf-8", index="partner")
-df_train.to_csv("data/data_train.csv", encoding="utf-8", index="partner")
-df_test.to_csv("data/data_test.csv", encoding="utf-8", index="partner")
+def get_transaction_data(year: int, month: int):
+    """
+    Returns transaction history from the beginning of the period up to and including the specified year and month
+    :param year: int, end year of the billing period
+    :param month: int, month of the end of the billing period
+    :return: Pandas Dataframe
+    """
+    end_of_period = get_end_of_period_date(year, month)
+    df_raw = pd.read_parquet("data/wallet_urfu.parquet.gzip")
+    df_dated = df_raw[df_raw['rep_date'] < end_of_period]
+    return df_dated
+
+
+def get_rfm_data(year: int, month: int):
+    """
+    Returns RFM data from the beginning of the period up to and including the specified year and month
+    :param year:
+    :param month:
+    :return:
+    """
+    df_transactions = get_transaction_data(year, month)
+    df_summary = summary_data_from_transaction_data(transactions=df_transactions, customer_id_col="partner",
+                                                    datetime_col="rep_date", monetary_value_col="monetary")
+    return df_summary
