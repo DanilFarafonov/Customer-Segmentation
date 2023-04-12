09abae90cd3beb1a23ebeda9809d212d2370979a
Danil_Farafonov <faraphonovd@yandex.ru>
2023-04-12 21:23:25 +0500

    fix functions

--- data_creation.py
+++ data_creation.py
@@ -6,10 +6,10 @@ from calendar import monthrange
 
 def get_end_of_period_date(year: int, month: int):
     """
-    Returns the end date of the billing period
+    Returns the end date of the billing period.
     :param year: int, end year of the billing period
     :param month: int, month of the end of the billing period
-    :return: datetime.date
+    :rtype: datetime.date
     """
     date_current = date(year, month, 1)
     days_in_current_month = monthrange(year, month)[1]
@@ -17,27 +17,40 @@ def get_end_of_period_date(year: int, month: int):
     return date_end
 
 
-def get_transaction_data(year: int, month: int):
+def get_transaction_history(year: int, month: int, df_transaction: pd.DataFrame):
     """
-    Returns transaction history from the beginning of the period up to and including the specified year and month
-    :param year: int, end year of the billing period
-    :param month: int, month of the end of the billing period
-    :return: Pandas Dataframe
+    Returns transaction history from the beginning of the period up to and including the specified year and month.
+    :param year: int,
+        end year of the billing period
+    :param month: int,
+        month of the end of the billing period
+    :param df_transaction: pd.DataFrame,
+        dataframe contains transaction history for the entire period
+    :rtype: pandas.DataFrame
     """
     end_of_period = get_end_of_period_date(year, month)
-    df_raw = pd.read_parquet("data/wallet_urfu.parquet.gzip")
-    df_dated = df_raw[df_raw['rep_date'] < end_of_period]
+    df_dated = df_transaction[df_transaction['rep_date'] < end_of_period]
     return df_dated
 
 
-def get_rfm_data(year: int, month: int):
+def get_transaction_alive_dead(year: int, month: int, df_transaction: pd.DataFrame):
+    pass
+
+
+def get_rfm_data(year: int, month: int, df_transaction: pd.DataFrame):
     """
-    Returns RFM data from the beginning of the period up to and including the specified year and month
-    :param year:
-    :param month:
-    :return:
+    Returns RFM data on two large clusters: alive and dead clients.
+    :param year: int,
+        end year of the billing period
+    :param month: int,
+        month of the end of the billing period
+    :param df_transaction: pd.DataFrame,
+        dataframe contains transaction history for the entire period
+    :rtype: tuple
     """
-    df_transactions = get_transaction_data(year, month)
-    df_summary = summary_data_from_transaction_data(transactions=df_transactions, customer_id_col="partner",
-                                                    datetime_col="rep_date", monetary_value_col="monetary")
-    return df_summary
+    df_alive, df_dead = get_transaction_alive_dead(year, month, df_transaction)
+    df_rfm_alive = summary_data_from_transaction_data(transactions=df_alive, customer_id_col="partner",
+                                                      datetime_col="rep_date", monetary_value_col="monetary")
+    df_rfm_dead = summary_data_from_transaction_data(transactions=df_dead, customer_id_col="partner",
+                                                     datetime_col="rep_date", monetary_value_col="monetary")
+    return df_rfm_alive, df_rfm_dead
